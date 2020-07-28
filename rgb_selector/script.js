d88a888dbe8044e6a9d8bcd11a5ccf6bbbc0c482
raphaelagaudard <workraphaelagaudard@gmail.com>
2020-07-28 10:00:43 -0300

    Código atualizado

--- rgb_selector/script.js
+++ rgb_selector/script.js
@@ -5,9 +5,11 @@ let textGreen = document.querySelector('#textGreen');
 let RangeBlue = document.querySelector('#RangeBlue');
 let textBlue = document.querySelector('#textBlue');
 let square = document.querySelector('#square');
+let ColorInputs = document.querySelector('.ColorInputs');
 
 function changeColor() {
   square.style.backgroundColor = `rgb(${RangeRed.value} ,  ${RangeGreen.value} , ${RangeBlue.value})`;
+  ColorInputs.style.border = `10px outset rgb(${RangeRed.value} ,  ${RangeGreen.value} , ${RangeBlue.value})`;
 }
 
 RangeRed.addEventListener('input', () => {
@@ -24,4 +26,3 @@ RangeBlue.addEventListener('input', () => {
   textBlue.value = RangeBlue.value;
   changeColor();
 });
-/*Fim*/
